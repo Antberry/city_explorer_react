6ca819df717a6d4aa405c5dea67d98a5831a2b1a
Antberry <aniej2013@gmail.com>
2019-05-09 10:31:44 -0700

    setup this.state

--- src/components/app.js
+++ src/components/app.js
@@ -7,11 +7,32 @@ import Map from './map';
 
 
   class App extends React.Component {
+    constructor(props){
+      super(props);
+
+      this.state = {
+        search_query: '',
+        formatted_query: '',
+        latitude: null,
+        longitude: null
+      }
+    }
+
+    updateLocation = (data) => {
+      this.setState({
+        search_query: data.search_query,
+        formatted_query: data.formatted_query,
+        latitude: data.latitude,
+        longitude: data.longitude
+
+      });
+    };
+
     render() {
       return (
         <React.Fragment>
           <Header />
-          <SearchForm/>
+          <SearchForm updateLocation = {this.updateLocation}/>
           <Map/>
           <SearchResults />
         </React.Fragment>
